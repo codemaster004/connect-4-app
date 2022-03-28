323224534cf63876f6f0fd7d5d8a5fb77db27677
Filip Dabkowski <filip.dabkowski@gmail.com>
2022-03-28 19:27:24 +0200

    Initial Commit

---
+++ Shared/Connect4App.swift
@@ -0,0 +1,17 @@
+//
+//  Connect4App.swift
+//  Shared
+//
+//  Created by Filip Dabkowski on 28/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Connect4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
